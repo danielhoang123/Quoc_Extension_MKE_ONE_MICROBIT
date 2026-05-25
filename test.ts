09e3733c78c1eb3d7f9f2341961f62b5d51ec5ad
danielhoang123 <48116508+danielhoang123@users.noreply.github.com>
2026-05-25 10:48:53 +0700

    Change test.ts

--- test.ts
+++ test.ts
@@ -1,8 +1,16 @@
 // tests go here; this will not be compiled when this package is used as an extension.
-basic.forever(function (){
-    if (LineTracker.eye3()) {
-        basic.showIcon(IconNames.Yes)
+basic.forever(function () {
+    if (LineTracker.eye1_detect()) {
+        basic.showString("1")
+    } else if (LineTracker.eye2_detect()) {
+        basic.showString("2")
+    } else if (LineTracker.eye3_detect()) {
+        basic.showString("3")
+    } else if (LineTracker.eye4_detect()) {
+        basic.showString("4")
+    } else if (LineTracker.eye5_detect()) {
+        basic.showString("5")
     } else {
-        basic.showIcon(IconNames.No)
+
     }
 })
