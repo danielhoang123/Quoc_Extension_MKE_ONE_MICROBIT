4c086580ab24bafc253e81e7038d1d020eed553c
danielhoang123 <48116508+danielhoang123@users.noreply.github.com>
2026-05-25 13:33:11 +0700

    Change test code to look similar to block code

--- test.ts
+++ test.ts
@@ -1,14 +1,14 @@
 // tests go here; this will not be compiled when this package is used as an extension.
 basic.forever(function () {
-    if (LineTracker.SensorPort.P1) {
+    if (LineTracker.detectLine(LineTracker.SensorPort.P1)) {
         basic.showString("1")
-    } else if (LineTracker.SensorPort.P2) {
+    } else if (LineTracker.detectLine(LineTracker.SensorPort.P2)) {
         basic.showString("2")
-    } else if (LineTracker.SensorPort.P3) {
+    } else if (LineTracker.detectLine(LineTracker.SensorPort.P3)) {
         basic.showString("3")
-    } else if (LineTracker.SensorPort.P4) {
+    } else if (LineTracker.detectLine(LineTracker.SensorPort.P4)) {
         basic.showString("4")
-    } else if (LineTracker.SensorPort.P5) {
+    } else if (LineTracker.detectLine(LineTracker.SensorPort.P5)) {
         basic.showString("5")
     } else {
 
